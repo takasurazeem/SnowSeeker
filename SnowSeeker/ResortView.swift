2a0c48413708b0b38933007ff76b2e7244d50dd7
Takasur Azeem <takasurazeem@gmail.com>
2022-08-06 01:19:14 +0500

    Adds skidetails and resortdetails view
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -18,6 +18,13 @@ struct ResortView: View {
                     .resizable()
                     .scaledToFit()
                 
+                HStack {
+                    ResortDetailsView(resort: resort)
+                    SkiDetailsView(resort: resort)
+                }
+                .padding(.vertical)
+                .background(Color.primary.opacity(0.1))
+                
                 Group {
                     Text(resort.description)
                         .padding(.vertical)
