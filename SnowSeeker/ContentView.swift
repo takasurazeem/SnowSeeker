b0536dea051ee63fbbcbc96f6b5f8642c2c97914
Takasur Azeem <takasurazeem@gmail.com>
2022-08-05 17:35:27 +0500

    Build primary list of items
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+        }
     }
 }
 
